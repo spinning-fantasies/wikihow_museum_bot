955fc6e55a0fab871d87a66ab02d5e38c7e3d0cb
Mathieu Mescam <mate@e.email>
2023-08-22 14:35:27 +0200

    fix hastag and move files when posted

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ load_dotenv()
 access_token = os.getenv("MASTODON_ACCESS_TOKEN")
 api_base_url = os.getenv("MASTODON_INSTANCE")
 
+filename = 'IMG_4100'
+
 # Read text from a file
-file_path = 'texts/IMG_4072.JPG.txt'
+file_path = f'texts/{filename}.JPG.txt'
 with open(file_path, 'r', encoding='utf-8') as file:
     text = file.read()
 
@@ -20,24 +22,26 @@ description = text
 cw_status = text
 
 # Upload the image first
-image_path = 'images/IMG_4072.JPG'
+image_path = f'images/{filename}.JPG'
 image_headers = {
     'Authorization': f'Bearer {access_token}'
 }
-image_files = {'file': ('images/IMG_4071.JPG.txt', open(image_path, 'rb'))}
+image_files = {'file': (f'images/{filename}.JPG', open(image_path, 'rb'))}
 image_response = requests.post(f'{api_base_url}/api/v1/media', headers=image_headers, files=image_files)
 image_data = json.loads(image_response.content)
 media_id = image_data.get('id')
 
 # Create the status payload
 status_payload = {
-    'status': cw_status,
+    'status': cw_status + "#WikihowMuseum",
     'media_ids': [media_id],
     'sensitive': True,
     'spoiler_text': status,
     'visibility': 'public',  # Set the visibility as needed
     'description': description
 }
+
+print(status_payload)
 
 # Post the status
 status_headers = {
@@ -48,5 +52,25 @@ status_response = requests.post(f'{api_base_url}/api/v1/statuses', headers=statu
 
 if status_response.status_code == 200:
     print("Status posted successfully!")
+    
+    text_source_path = f'texts/{filename}.JPG.txt'  # Replace with the actual source file path
+    image_source_path = f'images/{filename}.JPG'  # Replace with the actual source file path
+    destination_folder = 'posted'  # Replace with the actual destination folder path
+    
+    # Create the destination folder if it doesn't exist
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
+        
+    # Construct the full path for the destination
+    text_destination_path = os.path.join(destination_folder, os.path.basename(text_source_path))
+    image_destination_path = os.path.join(destination_folder, os.path.basename(image_source_path))
+
+    # Move the file to the destination
+    os.rename(text_source_path, text_destination_path)
+    print(f"File '{text_source_path}' moved to '{text_destination_path}'")
+
+    os.rename(image_source_path, image_destination_path)
+    print(f"File '{image_source_path}' moved to '{image_destination_path}'")
+
 else:
     print("Failed to post status:", status_response.content)
