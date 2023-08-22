d4c5288eeb34fb31cb215c264d792a451f22a704
Mathieu Mescam <mate@e.email>
2023-08-22 14:04:59 +0200

    read text from file, post with content warning & description

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import json
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -8,45 +9,44 @@ load_dotenv()
 access_token = os.getenv("MASTODON_ACCESS_TOKEN")
 api_base_url = os.getenv("MASTODON_INSTANCE")
 
-# upload test files to /api/v1/media
-IMAGES_FILES = [
+# Read text from a file
+file_path = 'texts/IMG_4072.JPG.txt'
+with open(file_path, 'r', encoding='utf-8') as file:
+    text = file.read()
 
-]
+# Create a new status (tweet) with content warning and description
+status = "Wikihow Museum, Meme"
+description = text
+cw_status = text
 
-files_root = Path("./images/")
-media_ids = []
-for file in IMAGES_FILES:
-    test_file = files_root / file
-    data = {
-        file
-    }
-    files = {
-        'file': (file, test_file.open('rb'), 'application/octet-stream')
-    }
-    url = "%s/api/v2/media" % (api_base_url)
-    r = requests.post(url, 
-        files=files, 
-        headers={'Authorization': 'Bearer %s' % (access_token)})
-    json_data = r.json()
+# Upload the image first
+image_path = 'images/IMG_4072.JPG'
+image_headers = {
+    'Authorization': f'Bearer {access_token}'
+}
+image_files = {'file': ('images/IMG_4071.JPG.txt', open(image_path, 'rb'))}
+image_response = requests.post(f'{api_base_url}/api/v1/media', headers=image_headers, files=image_files)
+image_data = json.loads(image_response.content)
+media_id = image_data.get('id')
 
-    media_id = json_data['id']
-    media_ids.append(media_id)
-
-# after collecting the media ids, include them in the toot payload
-data = { 
-    "status": """
-
-    #WikihowMuseum
-    """,
-    "description" : """
-    """,
-    "media_ids[]": media_ids,
-    "visibility": "public"
+# Create the status payload
+status_payload = {
+    'status': cw_status,
+    'media_ids': [media_id],
+    'sensitive': True,
+    'spoiler_text': status,
+    'visibility': 'public',  # Set the visibility as needed
+    'description': description
 }
 
-url = "%s/api/v1/statuses" % (api_base_url)
-r = requests.post(url, 
-        data=data, 
-        headers={'Authorization': 'Bearer %s' % (access_token)})
-json_data = r.json()
-print(json_data)
+# Post the status
+status_headers = {
+    'Authorization': f'Bearer {access_token}',
+    'Content-Type': 'application/json'
+}
+status_response = requests.post(f'{api_base_url}/api/v1/statuses', headers=status_headers, data=json.dumps(status_payload))
+
+if status_response.status_code == 200:
+    print("Status posted successfully!")
+else:
+    print("Failed to post status:", status_response.content)
