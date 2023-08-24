d546075ae604d3802b372958a4999b06036ccfe5
Mathieu Mescam <mate@e.email>
2023-08-24 18:14:19 +0200

    add text files content

--- example.py
+++ example.py
@@ -3,17 +3,21 @@ from pathlib import Path
 
 
 def main() -> int:
+    texts_path = Path("texts")
     images_path = Path("images")
 
     now = datetime.now(tz=UTC)
-    day_delta = 1
-    for image in images_path.iterdir():
-        data = {
-            "status": image,
-            "scheduled_at": now + timedelta(day_delta)
-        }
+    day_delta = 0
+    for text in texts_path.iterdir():
+        with open(text, 'r', encoding='utf-8') as file:
+            content = file.read()
+            data = {
+                "status": content,
+                # "status": text,
+                "scheduled_at": now + timedelta(day_delta)
+            }
         print(data)
-        day_delta += 1
+        day_delta += 0.5
 
     return 0
 
